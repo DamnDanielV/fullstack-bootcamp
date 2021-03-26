1a1ce06d33b1e8f69ed14b6380cda41a342071e1
DamnDanielV <1821@holbertonschool.com>
2021-03-25 19:21:57 -0500

    features: add new note and filter displayed elements

--- part2/bootcamp/src/App.js
+++ part2/bootcamp/src/App.js
@@ -1,17 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Note } from './Note';
 
 
-function App({ notes }) {
+function App( props ) {
+
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newNoteObject = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5 // true or false
+    }
+
+    setNotes(notes.concat(newNoteObject)) //change de state of notes and re-render it
+    //The method does not mutate the original notes array, but rather creates a new copy of the array with the new item added to the end. This is important since we must never mutate state directly in React!
+    setNewNote('') // clean the value of newNote and re-render it
+  }
+
+  const allNotes = showAll ? notes : notes.filter(note => note.important === true)
+
+
+  const handleChange = (e) => {
+    setNewNote(e.target.value)
+  }
+
   return (
    <div>
      <h1>Notes</h1>
-     {/* <ul>
-       {notes.map((note)=>{return <li key={note.id}>{note.content}</li>})}
-     </ul> */}
+    <div>
+      <button onClick={() => setShowAll(!showAll)}>{showAll ? 'important' : 'all'}</button>
+    </div>
      <ul>
-       {notes.map((note) => <Note key={ note.id } note={ note }/>)}
+       {allNotes.map((note) => <Note key={ note.id } note={ note }/>)}
      </ul>
+     <form onSubmit={handleSubmit}>
+       <input value={ newNote } onChange={handleChange}></input>
+       <button type="submit">Add new note</button>
+       
+     </form>
    </div>
   );
 }
